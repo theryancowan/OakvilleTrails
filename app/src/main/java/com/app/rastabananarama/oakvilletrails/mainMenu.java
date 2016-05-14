1a539519e39146e0de8bd3aeeb750b89d26d7022
Ryan <ryan_cowan@me.com>
2016-05-14 14:26:51 -0400

    hi

--- app/src/main/java/com/app/rastabananarama/oakvilletrails/mainMenu.java
+++ app/src/main/java/com/app/rastabananarama/oakvilletrails/mainMenu.java
@@ -1,7 +1,15 @@
 package com.app.rastabananarama.oakvilletrails;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Ryan on 16-05-14.
  */
-public class mainMenu {
+public class mainMenu extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu);
+    }
 }
