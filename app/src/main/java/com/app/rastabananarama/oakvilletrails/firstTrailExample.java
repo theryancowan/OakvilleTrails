d0b96c26bf0e765d819313e7d806f5be3fd8bdce
Ryan <ryan_cowan@me.com>
2016-05-15 15:25:55 -0400

    hi

--- app/src/main/java/com/app/rastabananarama/oakvilletrails/firstTrailExample.java
+++ app/src/main/java/com/app/rastabananarama/oakvilletrails/firstTrailExample.java
@@ -3,26 +3,22 @@ package com.app.rastabananarama.oakvilletrails;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.app.rastabananarama.oakvilletrails.R;
+import android.view.*;
 
 /**
  * Created by Ryan on 16-05-15.
  */
-public class findTrail extends Activity {
+public class firstTrailExample extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.find_a_trail);
+        setContentView(R.layout.first_trail_example);
 
     }
 
-
-    public void chosenTrail(View v) {
-        Intent intent = new Intent(findTrail.this, firstTrailExample.class);
+    public void clickbut(View v){
+        Intent intent = new Intent(firstTrailExample.this, MapsActivity.class);
         startActivity(intent);
-
     }
 }
